344ea7d27c39c02712986798cd4f2fa5254f5d76
realakashdeep <akashdeeppandey6313@gmail.com>
2024-02-29 14:11:05 +0530

    splash screen 2.0 Welcome Page

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,13 +1,39 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'welcome.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Timer(
+      Duration(seconds: 2),
+          () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => WelcomePage()),
+      ),
+    );
+
     return Scaffold(
+      backgroundColor: Colors.blue,
       body: Center(
-        child: Text(
-          'Splash Screen',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/local_hospital_outlined.svg', // Replace with your actual file name
+              width: 100,
+              height: 100,
+              color: Colors.white, // You can set the desired color
+            ),
+            SizedBox(height: 16),
+            Text(
+              'Digital HealthCare Application',
+              style: TextStyle(fontSize: 24, color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/screens/welcome.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome to'),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),  // Add some spacing between the title and text
+              Text(
+                'Digital HealthCare Facilities',
+                style: TextStyle(fontSize: 24),
+              ),
+              SizedBox(height: 80),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle log in button press
+                  // You can navigate to the login screen or perform any other action
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,  // Set button background color to blue
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),  // Set border radius
+                  ),
+                  minimumSize: Size(300, 40),
+                ),
+                child: Text('Log In', style: TextStyle(color: Colors.white, fontSize: 20)),
+              ),
+              SizedBox(height: 12),  // Add spacing between buttons
+              ElevatedButton(
+                onPressed: () {
+                  // Handle sign up button press
+                  // You can navigate to the sign-up screen or perform any other action
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,  // Set button background color to blue
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),  // Set border radius
+                  ),
+                  minimumSize: Size(300, 40),
+                ),
+                child: Text('Sign up',style: TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+              Spacer(),  // Fill the available space between buttons and "Skip" text
+              GestureDetector(
+                onTap: () {
+                  // Handle the "Skip" action
+                },
+                child: Text(
+                  'Skip',
+                  style: TextStyle(fontSize: 20, color: Colors.blue),
+                ),
+              ),
+              SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
