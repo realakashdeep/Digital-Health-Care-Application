89bf86087485e9a418b1a8dda3dfe98d8dcba295
realakashdeep <akashdeeppandey6313@gmail.com>
2024-03-06 21:55:11 +0530

    added- validation of textFields with requirements.

--- lib/screens/user/user_login.dart
+++ lib/screens/user/user_login.dart
@@ -1,10 +1,10 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:final_year_project/screens/user/user_home.dart';
 
 class UserLogin extends StatelessWidget {
-  const UserLogin({super.key});
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  UserLogin({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,69 +12,82 @@ class UserLogin extends StatelessWidget {
       resizeToAvoidBottomInset: false,
       body: Container(
         margin: EdgeInsets.zero,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(
-              height: 300,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/login_user_image.jpg'),
-                  fit: BoxFit.cover,
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 300,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/login_user_image.jpg'),
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 12),
-            SizedBox(
-              child: Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 40,
-                  fontFamily: 'Roboto',
-                  fontWeight: FontWeight.w400,
+              const SizedBox(height: 12),
+              SizedBox(
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 12),
-            userCred(context),
-            const SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => UserHome()),
-                  );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+              const SizedBox(height: 12),
+              userCred(context),
+              const SizedBox(height: 12),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => UserHome()),
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  minimumSize: Size(300, 40),
                 ),
-                minimumSize: Size(300, 40),
+                child: Text('Log In', style: TextStyle(color: Colors.white, fontSize: 20)),
               ),
-              child: Text('Log In', style: TextStyle(color: Colors.white, fontSize: 20)),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
-  userCred(context) {
+  Widget userCred(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(left: 20, right: 20),
+      margin: EdgeInsets.only(left: 20, right: 20),
       child: Column(
         children: [
           SizedBox(
             width: 300,
             height: 50,
-            child: TextField(
+            child: TextFormField(
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter User ID/Phone Number';
+                }
+                if (!RegExp(r'^[0-9]{10}$').hasMatch(value)) {
+                  return 'Please enter a valid phone number';
+                }                return null;
+              },
               decoration: InputDecoration(
                 labelText: 'Enter User ID/Phone Number',
                 hintText: 'Enter User ID/Phone Number',
-                hintStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                hintStyle: TextStyle(color: Colors.grey),
                 alignLabelWithHint: true,
-                contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),  // Adjust vertical and horizontal padding as needed
+                contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
@@ -85,14 +98,31 @@ class UserLogin extends StatelessWidget {
           SizedBox(
             width: 300,
             height: 50,
-            child: TextField(
-              obscureText: true,  // Use obscureText for password fields
+            child: TextFormField(
+              obscureText: true,
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter your password';
+                }
+                if (value.contains(' ')) {
+                  return 'Password cannot contain spaces';
+                }
+
+                if (value.length > 32) {
+                  return 'Password cannot be longer than 32 characters';
+                }
+
+                if (value.length < 8 ) {
+                  return 'Password cannot be less than 8 characters';
+                }
+                return null;
+              },
               decoration: InputDecoration(
                 labelText: 'Password',
                 hintText: 'Enter Password',
                 hintStyle: TextStyle(fontSize: 14, color: Colors.grey),
                 alignLabelWithHint: true,
-                contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),  // Adjust vertical and horizontal padding as needed
+                contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(10.0),
                 ),
