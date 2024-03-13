9f2d935628a870d92e2b72032d37b6b49b019ac2
realakashdeep <akashdeeppandey6313@gmail.com>
2024-03-14 03:18:03 +0530

    added: validation. updated design(textField height and width) functionality(button will now go upwards relatively when keyboard pops up)

--- lib/screens/ward/ward_login.dart
+++ lib/screens/ward/ward_login.dart
@@ -1,112 +1,85 @@
 import 'package:final_year_project/screens/ward/ward_menu.dart';
 import 'package:flutter/material.dart';
 
-class WardLoginPage extends StatefulWidget {
-  @override
-  _WardLoginPageState createState() => _WardLoginPageState();
-}
-
-class _WardLoginPageState extends State<WardLoginPage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _wardIdController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-
+class WardLoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true, // Allow the widget to resize when the keyboard appears
       appBar: AppBar(
         title: Text('Digital HealthCare Facilities'),
         centerTitle: true,
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(height: 22),
-                  Text(
-                    'Ward Login',
-                    style: TextStyle(fontSize: 24),
-                  ),
-                  SizedBox(height: 80),
-                  buildTextField(_wardIdController, 'Ward ID', 'Enter Ward ID'),
-                  SizedBox(height: 16),
-                  buildTextField(_passwordController, 'Password', 'Enter Ward Password', obscureText: true),
-                  SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (_formKey.currentState!.validate()) {
-                        // Perform login logic here
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => WardMenuPage()),
-                        );
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.blue,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      minimumSize: Size(300, 40),
-                    ),
-                    child: Text('Log In', style: TextStyle(color: Colors.white, fontSize: 20)),
-                  ),
-                ],
+        child: Align(
+          alignment: Alignment.topCenter,
+          //padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 22),
+              Text(
+                'Ward Login',
+                style: TextStyle(fontSize: 24),
               ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildTextField(TextEditingController controller, String labelText, String hintText, {bool obscureText = false}) {
-    return Container(
-      margin: EdgeInsets.only(left: 45, right: 45),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10.0),
-        color: Colors.white,
-      ),
-      child: TextFormField(
-        controller: controller,
-        obscureText: obscureText,
-        validator: (value) {
-          if (value == null || value.isEmpty) {
-            return 'Please enter $labelText';
-          }
-          RegExp wardIdRegExp = RegExp(r'^[A-Z]{3}\d{6}$');
-          if (!wardIdRegExp.hasMatch(value) && controller == _wardIdController) {
-            return 'Invalid Ward ID format';
-          }
-          else if(controller == _passwordController){
-            if (value.contains(' ')) {
-              return 'Password cannot contain spaces';
-            }
-
-            if (value.length > 32) {
-              return 'Password cannot be longer than 32 characters';
-            }
-
-            if (value.length < 8) {
-              return 'Password cannot be less than 8 characters';
-            }
-          }
-          return null;
-        },
-        decoration: InputDecoration(
-          labelText: labelText,
-          hintText: hintText,
-          hintStyle: TextStyle(fontSize: 14, color: Colors.grey),
-          alignLabelWithHint: true,
-          contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10.0),
+              SizedBox(height: 80),  // Adjust the spacing based on your design
+              Container(
+                width: 300,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Ward ID',
+                    hintText: 'Enter Ward ID',
+                    hintStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                    alignLabelWithHint: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Container(
+                width: 300,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.0),
+                  color: Colors.white,
+                ),
+                child: TextField(
+                  obscureText: true,  // Use obscureText for password fields
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter Ward Password',
+                    hintStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                    alignLabelWithHint: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => WardMenuPage()), // Navigate to WardMenuPage
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  minimumSize: Size(300, 40),
+                ),
+                child: Text('Log In', style: TextStyle(color: Colors.white, fontSize: 20)),
+              ),
+            ],
           ),
         ),
       ),
