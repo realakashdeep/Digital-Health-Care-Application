ea1530722d8414117f1a0b8e0905e2b9f88f600b
realakashdeep <akashdeeppandey6313@gmail.com>
2024-04-09 16:14:04 +0530

    updated functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:final_year_project/screens/user/user_signup.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:final_year_project/provider/auth_provider.dart' as MyAppAuthProvider;
 import 'package:provider/provider.dart';
 
+import 'screens/splash_screen.dart';
 import 'firebase_options.dart';
 
-Future <void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
