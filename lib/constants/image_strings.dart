99abf064c9c61b5cc8a96a44b920ccb9fab4fb40
Sushantlama <lsushant009@gmail.com>
2024-05-19 12:46:04 +0530

    constants used in the app

---
+++ lib/constants/image_strings.dart
@@ -0,0 +1,3 @@
+
+const String userLoginImg = "assets/login_user_image.jpg";
+const String ImgProfile = "assets/rwd.jpeg";
---
+++ lib/constants/text_strings.dart
@@ -0,0 +1,30 @@
+
+//titles
+const String tTitle = "Digital Health Application";
+const String tProfile = "Profile";
+const String tRegistration = "Registration";
+const String tHome = "Home";
+const String tOtp = "OTP Verification";
+
+
+//Labels
+const String tName = "Name";
+const String tPhoneNumber = "PhoneNumber";
+const String tAddress = "Address";
+const String tGender = "Gender";
+const String tAadharNumber = "Aadhar Number";
+const String tPassword = "Password";
+
+
+
+
+
+//buttons
+const String tSubmit = "Submit";
+const String tLogOut = "Log Out";
+const String tEditProfile = "Edit Profile";
+const String tSaveProfile = "Save Profile";
+const String tLogin = "Login";
+const String tSignUp = "SignUp";
+const String tSkip = "Skip";
+const String tWardLogin = "Ward Login";
