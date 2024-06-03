18f282dacf9d80c856f481bbc404efa7b093917a
realakashdeep <akashdeeppandey6313@gmail.com>
2024-06-03 18:25:47 +0530

    updated design and functionality of Care Givers and Doctors.

--- lib/provider/care_givers_auth_provider.dart
+++ lib/provider/care_givers_auth_provider.dart
@@ -1,8 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 
 class CareGiversAuthProvider extends ChangeNotifier {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
   User? _user;
 
   User? get user => _user;
@@ -14,6 +16,16 @@ class CareGiversAuthProvider extends ChangeNotifier {
         password: password,
       );
       _user = userCredential.user;
+
+      // Check if the user exists in Firestore
+      DocumentSnapshot<Map<String, dynamic>> snapshot = await _firestore.collection('caregivers').doc(_user!.uid).get();
+      if (snapshot.exists) {
+        // If user exists and email is null, update the email in Firestore
+        if (snapshot.data()!['email'] == null) {
+          await _firestore.collection('caregivers').doc(_user!.uid).update({'email': email});
+        }
+      }
+
       notifyListeners();
     } catch (e) {
       throw e;
