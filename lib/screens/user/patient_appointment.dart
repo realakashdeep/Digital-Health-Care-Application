a5be38c4653495b2f16e4a18311be098ba9a0ebf
realakashdeep <akashdeeppandey6313@gmail.com>
2024-06-08 22:29:21 +0530

    updated dependencies.

--- lib/screens/user/patient_appointment.dart
+++ lib/screens/user/patient_appointment.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../../../../models/appointment_model.dart';
+import 'AppointmentDetails.dart';  // Import the new page
 
 class PatientAppointmentReport extends StatefulWidget {
   @override
@@ -18,20 +19,19 @@ class _PatientAppointmentReportState extends State<PatientAppointmentReport> {
   }
 
   Future<void> _fetchAppointments() async {
-
     try {
       User? currentUser = FirebaseAuth.instance.currentUser;
-      if(currentUser!= null){
-          final QuerySnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
-              .collection('appointments')
-              .where('patientId', isEqualTo: currentUser.uid)
-              .get();
+      if (currentUser != null) {
+        final QuerySnapshot<Map<String, dynamic>> snapshot = await FirebaseFirestore.instance
+            .collection('appointments')
+            .where('patientId', isEqualTo: currentUser.uid)
+            .get();
 
-          final appointments = snapshot.docs.map((doc) => AppointmentModel.fromSnapshot(doc)).toList();
+        final appointments = snapshot.docs.map((doc) => AppointmentModel.fromSnapshot(doc)).toList();
 
-          setState(() {
-            _appointments = appointments;
-          });
+        setState(() {
+          _appointments = appointments;
+        });
       }
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -48,53 +48,45 @@ class _PatientAppointmentReportState extends State<PatientAppointmentReport> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: DataTable(
-                    columns: const [
-                      DataColumn(label: Text('Patient Name')),
-                      DataColumn(label: Text('Appointment Date')),
-                      DataColumn(label: Text('BP')),
-                      DataColumn(label: Text('Temperature')),
-                      DataColumn(label: Text('Heart Rate')),
-                      DataColumn(label: Text('SpO2')),
-                      DataColumn(label: Text('Assigned To')),
-                      DataColumn(label: Text('Doctor Mail')),
-                      DataColumn(label: Text('Care Mail')),
-                      DataColumn(label: Text('Ward Number')),
-                      DataColumn(label: Text('Status')),
-                      DataColumn(label: Text('Symptoms')),
-                      DataColumn(label: Text('Prescriptions')),
-                      DataColumn(label: Text('Tests')),
-                    ],
-                    rows: _appointments.map((appointment) {
-                      return DataRow(cells: [
-                        DataCell(Text(appointment.patientName)),
-                        DataCell(Text(appointment.appointmentDate)),
-                        DataCell(Text(appointment.bp)),
-                        DataCell(Text(appointment.temp)),
-                        DataCell(Text(appointment.heartRate)),
-                        DataCell(Text(appointment.spO2)),
-                        DataCell(Text(appointment.assignedTo)),
-                        DataCell(Text(appointment.doctorMail)),
-                        DataCell(Text(appointment.careMail)),
-                        DataCell(Text(appointment.wardNumber)),
-                        DataCell(Text(appointment.status)),
-                        DataCell(Text(appointment.symptoms)),
-                        DataCell(Text(appointment.prescriptions)),
-                        DataCell(Text(appointment.tests)),
-                      ]);
-                    }).toList(),
-                  ),
+        child: _appointments.isEmpty
+            ? Center(
+          child: Text(
+            'No reports till now',
+            style: TextStyle(fontSize: 18),
+          ),
+        )
+            : ListView.builder(
+          itemCount: _appointments.length,
+          itemBuilder: (context, index) {
+            final appointment = _appointments[index];
+            return Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              child: ListTile(
+                contentPadding: EdgeInsets.all(16.0),
+                title: Text(
+                  appointment.patientName,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Appointment Date: ${appointment.appointmentDate}'),
+                    Text('Assigned To: ${appointment.assignedTo}'),
+                  ],
+                ),
+                trailing: Icon(Icons.arrow_forward),
+                onTap: () {
+                  print(appointment);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => AppointmentDetails(appointment: appointment),
+                    ),
+                  );
+                },
               ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
